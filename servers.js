a683f2cc562abe55c630c9fae66d7a40602afa7c
Sarvo178 <ovindupathirana554@gmail.com>
2024-11-18 12:05:53 +0530

    new

--- servers.js
+++ servers.js
@@ -9,7 +9,7 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
   cors: {
-    origin: "http://localhost:3001",
+    origin: "https://frontend-fvyq.onrender.com",
     methods: ["GET", "POST"]
   }
 });
